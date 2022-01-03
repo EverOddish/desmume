9d7a6422ed94eb6aaff1bffd113ef2ed795e3dc3
EverOddish <everoddish@gmail.com>
2022-01-03 09:48:53 -0500

    Added spelling correction

--- desmume/src/screenReader.cpp
+++ desmume/src/screenReader.cpp
@@ -4,6 +4,8 @@
 #include "screenReader.h"
 #include "chatot_lib.h"
 
+static uint64_t g_stepCounter = 0;
+
 void InitializeScreenReader()
 {
 	ChatotLib_Initialize();
@@ -11,11 +13,14 @@ void InitializeScreenReader()
 
 void UpdateScreenReader(const NDSDisplayInfo& displayInfo)
 {
-	std::string text;
-	CLColourFormat format = BGR555;
-
-	switch(displayInfo.colorFormat)
+	// Perform OCR every 5 steps
+	if (0 == g_stepCounter % 5)
 	{
+		std::string text;
+		CLColourFormat format = BGR555;
+
+		switch (displayInfo.colorFormat)
+		{
 		case NDSColorFormat_BGR555_Rev:
 			format = BGR555;
 			break;
@@ -25,22 +30,30 @@ void UpdateScreenReader(const NDSDisplayInfo& displayInfo)
 		case NDSColorFormat_BGR888_Rev:
 			format = BGR888;
 			break;
-	}
-
-	for (int i = 0; i < 2; i++)
-	{
-		if (displayInfo.didPerformCustomRender[i])
-		{
-			ChatotLib_GetTextFromScreen(displayInfo.customBuffer[i], displayInfo.customWidth, displayInfo.customHeight, format, text);
-		}
-		else
-		{
-			ChatotLib_GetTextFromScreen(displayInfo.nativeBuffer16[i], GPU_FRAMEBUFFER_NATIVE_WIDTH, GPU_FRAMEBUFFER_NATIVE_HEIGHT, format, text);
 		}
 
-		if (text.length() > 0)
+		for (int i = 0; i < 2; i++)
 		{
-			std::cout << text << std::endl;
+			if (displayInfo.didPerformCustomRender[i])
+			{
+				ChatotLib_GetTextFromScreen(displayInfo.customBuffer[i], displayInfo.customWidth, displayInfo.customHeight, format, text);
+			}
+			else
+			{
+				ChatotLib_GetTextFromScreen(displayInfo.nativeBuffer16[i], GPU_FRAMEBUFFER_NATIVE_WIDTH, GPU_FRAMEBUFFER_NATIVE_HEIGHT, format, text);
+			}
+
+			if (text.length() > 0)
+			{
+				std::cout << "Original text: " << text << std::endl;
+
+				std::string outText;
+				ChatotLib_CorrectText(text, outText);
+
+				std::cout << "Correctd text: " << outText << std::endl;
+			}
 		}
 	}
+
+	g_stepCounter++;
 }
